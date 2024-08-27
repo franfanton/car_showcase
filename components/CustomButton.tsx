a3edd8d5fc33cfd031cad9351f730aba04a88350
franfanton <franfanton92@gmail.com>
2024-08-27 18:41:10 -0300

    add icons, configurations and hero banners

---
+++ components/CustomButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+import { CustomButtonProps } from "@/types";
+
+function CustomButton({
+  title,
+  containerStyles,
+  handleClick,
+}: Readonly<CustomButtonProps>) {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={"flex-1"}> {title}</span>
+    </button>
+  );
+}
+
+export default CustomButton;
